dfb5e85f2f1ee1b8912fb2bf757b167e9578b84e
sumansingh226 <sumansingh13062000@gmail.com>
2024-04-10 22:25:46 +0530

    jwt decode

--- src/Middlewares/decodeJwt.ts
+++ src/Middlewares/decodeJwt.ts
@@ -1,36 +1,34 @@
-import { Request, Response, NextFunction } from 'express';
-import jwt, { VerifyErrors } from 'jsonwebtoken';
+import { Request, Response, NextFunction } from "express";
+import jwt, { VerifyErrors } from "jsonwebtoken";
 
-// Middleware to decode JWT and check role
-export function decodeJwtAndCheckRole(role: string) {
-    return function (req: Request, res: Response, next: NextFunction) {
-        // Get the JWT from the Authorization header
+const isUserAuthnticated = (role: string) => {
+    return function (req: Request | any, res: Response, next: NextFunction) {
         const authHeader = req.headers.authorization;
         if (!authHeader) {
-            return res.status(401).json({ message: 'Authorization header is missing' });
+            return res
+                .status(401)
+                .json({ message: "Authorization header is missing" });
         }
 
-        const token = authHeader.split(' ')[1];
+        const token = authHeader.split(" ")[1];
         if (!token) {
-            return res.status(401).json({ message: 'JWT token is missing' });
+            return res.status(401).json({ message: "JWT token is missing" });
         }
 
-        // Secret key used to sign the token
-        const secretKey = 'your_secret_key';
-
-        // Decode the JWT
+        const secretKey = "your_secret_key";
         jwt.verify(token, secretKey, (err: VerifyErrors | null, decoded: any) => {
             if (err) {
-                return res.status(401).json({ message: 'Invalid JWT token' });
+                return res.status(401).json({ message: "Invalid JWT token" });
             }
 
             if (decoded.role !== role) {
-                return res.status(403).json({ message: 'Unauthorized access' });
+                return res.status(403).json({ message: "Unauthorized access" });
             }
 
             req.user = decoded;
-
             next();
         });
     };
-}
+};
+
+export default isUserAuthnticated;
