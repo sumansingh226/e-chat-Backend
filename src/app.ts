48dea2d8de9a16ed8426967435e7570fbb2c95d8
sumansingh226 <sumansingh13062000@gmail.com>
2024-04-11 21:49:22 +0530

    jwt decode

--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,8 @@
 import express, { Request, Response } from 'express';
 import http from 'http';
-import { Server as SocketServer } from 'socket.io';
-import { graphqlHTTP } from 'express-graphql';
-import { buildSchema } from 'graphql';
 import Database from './config/dbconnection';
 
 const app = express();
-const server = http.createServer(app);
-const io = new SocketServer(server);
-
-
-
-// Root resolver
-const root = {
-    hello: () => 'Hello, world!'
-};
-
-
-// Socket.io connection
-io.on('connection', (socket) => {
-    console.log('A user connected');
-    socket.on('disconnect', () => {
-        console.log('User disconnected');
-    });
-});
-
 
 const uri = 'mongodb://localhost:27017';
 const dbName = 'e-chat';
@@ -32,13 +10,17 @@ const dbName = 'e-chat';
 async function initialize() {
     const database = Database.getInstance();
     await database.connect(uri, dbName);
-
-    const db = database.getDb();
 }
 
 initialize();
+
 // Express server
 const PORT = 3000;
-server.listen(PORT, () => {
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello, World!');
+});
+
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
