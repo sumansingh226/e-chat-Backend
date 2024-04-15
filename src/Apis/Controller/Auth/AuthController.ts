fc9fe5300111f8fb4813842618b0b449c28f4ad8
sumansingh226 <sumansingh13062000@gmail.com>
2024-04-16 00:20:12 +0530

    nodemailer

--- src/Apis/Controller/Auth/AuthController.ts
+++ src/Apis/Controller/Auth/AuthController.ts
@@ -171,6 +171,73 @@ export const resetPassword = async (req: Request, res: Response) => {
     }
 };
 
+const sendPasswordResetEmail = async (toEmail: string, resetToken: string) => {
+    const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            user: process.env.EMAIL_ID,
+            pass: process.env.Pass,
+        },
+    });
+
+    const resetLink = `http://localhost:3000/update-password?token=${resetToken}`;
+    const mailOptions = {
+        from: process.env.EMAIL_ID,
+        to: toEmail,
+        subject: "Password Reset - E-shop",
+        html: `
+        <html>
+        <head>
+            <style>
+                body {
+                    font-family: 'Arial', sans-serif;
+                    color: #333;
+                    margin: 20px;
+                }
+                h2 {
+                    color: #0066cc;
+                }
+                p {
+                    margin-bottom: 15px;
+                }
+                a {
+                    color: #0066cc;
+                    text-decoration: none;
+                }
+                a:hover {
+                    text-decoration: underline;
+                }
+                .container {
+                    border: 1px solid #ddd;
+                    padding: 20px;
+                    border-radius: 5px;
+                    background-color: #f9f9f9;
+                }
+            </style>
+        </head>
+        <body>
+            <div class="container">
+                <h2>Hi there!</h2>
+                <p>We received a request to reset your password for E-shop.</p>
+                <p>Click the following link to reset your password:</p>
+                <a href="${resetLink}">${resetLink}</a>
+                <p>If you did not request a password reset, please ignore this email.</p>
+                <p>Best regards,</p>
+                <p>The E-shop Team</p>
+            </div>
+        </body>
+    </html>
+
+        `,
+    };
+
+    try {
+        await transporter.sendMail(mailOptions);
+        console.log("Password reset email sent successfully.");
+    } catch (error) {
+        console.error("Error sending password reset email:", error);
+    }
+};
 
 export const forgotPassword = async (req: Request | any, res: Response) => {
     const { email } = req.body;
